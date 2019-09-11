d0d50f5843f133e060e0a4f453750fd4a89857fb
GalinaSPK <galina@code-stable.com>
2019-09-11 14:51:02 +0500

    test

--- src/MainClassTest.java
+++ src/MainClassTest.java
@@ -7,6 +7,6 @@ public class MainClassTest
     {
         int num = MainClass.getLocalNumber();
         Assert.assertEquals("Проверка метода getLocalNumber провалена", 14, num);
-        System.out.println("Test successful");
+        System.out.println("Проверка успешна");
     }
 }
